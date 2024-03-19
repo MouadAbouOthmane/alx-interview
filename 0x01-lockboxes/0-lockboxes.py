eb1ab3122c5aeee944f019b0cc43998a59e4b4af
Mouad Abou Othmane <104225906+MouadAbouOthmane@users.noreply.github.com>
2024-03-19 09:57:01 +0000

    Update 0-lockboxes.py

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -1,15 +1,17 @@
 #!/usr/bin/python3
-"""Solves the lock boxes puzzle """
-
+"""
+Solves the lock boxes puzzle
+"""
 
 def canUnlockAll(boxes):
-    """Check if all boxes can be opened
+    """
+    Check if all boxes can be opened
     Args:
         boxes (list): List which contain all the boxes with the keys
     Returns:
         bool: True if all boxes can be opened, otherwise, False
     """
-        if not boxes:
+    if not boxes:
         return True
 
     num_boxes = len(boxes)
